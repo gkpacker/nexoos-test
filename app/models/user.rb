7374923fa5bff3facfa8e18719bf872d599229b2
Gabriel Packer <ggkpacker@gmail.com>
2018-06-06 13:57:38 -0300

    Validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, :phone_number, presence: true
+  validates :phone_number, format: { with: /\d{4}-\d{4}/,
+    message: "Phone number doesn't match valid format (XXXX-XXXX)" }
 end
